6b5bb3d8bfa6f514c6761ff271426a6944008bac
sanix sandel <sanicksikani@gmail.com>
2020-08-24 01:11:39 +0300

    first commit

--- src/main/java/com/restfulservice/CRUD/CrudApplication.java
+++ src/main/java/com/restfulservice/CRUD/CrudApplication.java
@@ -2,7 +2,9 @@ package com.restfulservice.CRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.restfulservice")
 @SpringBootApplication
 public class CrudApplication {
 
